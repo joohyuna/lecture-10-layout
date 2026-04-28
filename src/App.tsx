65196326aec46b3cdf270d8bc73da791f35f7b8b
mcm <joohyuna2@gmail.com>
2026-04-28 15:42:07 +0900

    localStorage 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,34 @@ import GlobalStyle from "./styles/GlobalStyle.ts";
 import { AppRouter } from "./router/AppRouter.tsx";
 import { ThemeProvider } from "styled-components";
 import { DarkTheme, LightTheme } from "./styles/theme.ts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Provider 제공자 ThemeProvider테마 제공자, <RouterProvider 라우터제공자, 계속 보게 될것임
 // theme={LightTheme} 원래의 Default Theme 가 들어가야 하는 자리인데 재지정 해서 들어간 자리
 // ThemeProvider로 감싸진 자식 컴포넌트들 안에서 사용되는 styled-components 들에게 해당 태마를 제공함
 function App() {
-    const [theme, setTheme] = useState<"light" | "dark">("light");
+    // 초기값 잘에 함수를 집어 넣을 수도 잇음,
+    // useState가 state를 만들 때 그 함 수를 실행 시키고 난 결과를 초기값으로 사용함
+    // 내가 처음 사용하는 상태에서는 값을 없다 그러므로 무조건 null | string
+    const [theme, setTheme] = useState<"light" | "dark">(() => {
+        const savedThema = localStorage.getItem("theme");
+        return savedThema === "dark" ? "dark" : "light";
+        // dark가 아니면 무조건 라이트
+        // dark 외의 경우에는 무조건 light가 찍힌다.
+    });
+
+
+   // state를 기본으로 상요하고, 그데치터의 추가 저장소로 localStorage를 사용할 뿐
+   // 1. 사용자가 state값을 바꿧을때
+
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+    }, [theme]);
 
     const onClick = () => {
-        if (theme === "light") {
-            setTheme("dark");
-        } else {
-            setTheme("light");
-        }
+        // setState에도 매개변수로 함수를 작성해줄수 있는데,
+        // 이 때 함수의 매개변수 자리에는 "지금 현재 state 값"이 들어옴
+        setTheme(prev => prev === "dark" ? "light" : "dark");
     };
     return (
         <ThemeProvider theme={theme === "light" ? LightTheme : DarkTheme}>
--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,7 +1,17 @@
+import styled from "styled-components";
 
+const LogInWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100dvh;
+    background-color: ${props => props.theme.color.background};
+    color: ${props => props.theme.color.text};
+`;
 
 function LogIn() {
-    return <>로그인 화면입니다.</>;
+    return <LogInWrap>
+        로그인 화면입니다.
+    </LogInWrap>;
 }
 
 export default LogIn;
